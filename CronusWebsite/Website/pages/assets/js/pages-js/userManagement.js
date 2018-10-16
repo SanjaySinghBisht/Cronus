436532fabbc33abb95377ac7632f1777145adeeb
libra89sanjay@gmail.com <libra89sanjay@gmail.com>
2018-10-16 10:41:30 +0530

    user management js changes

--- CronusWebsite/Website/pages/assets/js/pages-js/userManagement.js
+++ CronusWebsite/Website/pages/assets/js/pages-js/userManagement.js
@@ -9,72 +9,95 @@ $(document).ready(function() {
   }); // Second table
 
   stockTable.on("click", "tbody tr td input", function() {
-   if($(this).is(":checked")){
-     $(this).parents("tr").addClass("selected");
-  }
-  else{
-    $(this).parents("tr").removeClass("selected"); 
-   }
+    if ($(this).is(":checked")) {
+      $(this)
+        .parents("tr")
+        .addClass("selected");
+    } else {
+      $(this)
+        .parents("tr")
+        .removeClass("selected");
+    }
   });
   catalogTable.on("click", "tbody tr td input", function() {
-    if($(this).is(":checked")){
-      $(this).parents("tr").addClass("selected");
+    if ($(this).is(":checked")) {
+      $(this)
+        .parents("tr")
+        .addClass("selected");
+    } else {
+      $(this)
+        .parents("tr")
+        .removeClass("selected");
     }
-    else{
-      $(this).parents("tr").removeClass("selected"); 
+  });
+  $("#btnRight").on("click", function() {
+    if ($(".non-group-username tr td input:checked").length > 0) {
+      swal({
+        title: "Are you sure to move selected user?",
+        text: "User will be shifted from Non group to group",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+      }).then(willDelete => {
+        if (willDelete) {
+          moveRows(catalogTable, stockTable);
+          swal("User is successfully shifted to group", {
+            icon: "success"
+          });
+        } else {
+          swal("User is not shifted to group");
+        }
+      });
+    } else {
+      swal({
+        title: "Error !!!",
+        text: "Please select records to be moved",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+      });
     }
   });
-  $('#btnRight').on('click',function () {
-    swal({
-      title: "Are you sure to move selected user?",
-      text: "User will be shifted from Non group to group",
-      icon: "warning",
-      buttons: true,
-      dangerMode: true,
-    })
-    .then((willDelete) => {
-      if (willDelete) {
-        moveRows(catalogTable, stockTable);
-        swal("User is successfully shifted to group", {
-          icon: "success",
-        });
-      } else {
-        swal("User is not shifted to group");
-      }
-    });
+
+  $("#btnLeft").on("click", function() {
+    if ($(".group-username tr td input:checked").length > 0) {
+      swal({
+        title: "Are you sure to move selected user?",
+        text: "User will be shifted from group to Non group",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+      }).then(willDelete => {
+        if (willDelete) {
+          moveRows(stockTable, catalogTable);
+          swal("User is successfully shifted to Non group", {
+            icon: "success"
+          });
+        } else {
+          swal("User is not shifted to Non group");
+        }
+      });
+    } else {
+      swal({
+        title: "Error !!!",
+        text: "Please select records to be moved",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+      });
+    }
   });
-   
-  $('#btnLeft').on('click',function () {
-    swal({
-      title: "Are you sure to move selected user?",
-      text: "User will be shifted from group to Non group",
-      icon: "warning",
-      buttons: true,
-      dangerMode: true,
-    })
-    .then((willDelete) => {
-      if (willDelete) {
-        moveRows(stockTable, catalogTable);
-        swal("User is successfully shifted to Non group", {
-          icon: "success",
-        });
-      } else {
-        swal("User is not shifted to Non group");
-      }
-    });
-   
-  });
-  function moveRows(fromTable, toTable){
-    var $row= fromTable.find(".selected");
-    $.each($row, function(k, v){
-      if(this != null){
+  function moveRows(fromTable, toTable) {
+    var $row = fromTable.find(".selected");
+    $.each($row, function(k, v) {
+      if (this != null) {
         addRow = fromTable.fnGetData(this);
         // toTable.fnAddData(addRow);
         var a = toTable.fnAddData(addRow);
         var oSettings = toTable.fnSettings();
-        var nTr = oSettings.aoData[ a[0] ].nTr;
-        nTr.setAttribute("class","swappedRow");
-        fromTable.fnDeleteRow(this);  
+        var nTr = oSettings.aoData[a[0]].nTr;
+        nTr.setAttribute("class", "swappedRow");
+        fromTable.fnDeleteRow(this);
       }
     });
   }
@@ -85,7 +108,7 @@ $(document).ready(function() {
     ["SBisht", "Sanjay Singh Bisht", "sanjay.bisht@pimco.com"],
     ["SBisht", "Sanjay Singh Bisht", "sanjay.bisht@pimco.com"]
   ];
-  
+
   var container = document.getElementById("users-grid");
   var hot = new Handsontable(container, {
     data: dataObject,
@@ -134,20 +157,20 @@ $(document).ready(function() {
       scrollY: "400px",
       dom: "Bfrtip",
       select: true,
-        buttons: [
-            {
-                text: 'Select all',
-                action: function () {
-                    table.rows().select();
-                }
-            },
-            {
-                text: 'Select none',
-                action: function () {
-                    table.rows().deselect();
-                }
-            }
-        ],
+      buttons: [
+        {
+          text: "Select all",
+          action: function() {
+            table.rows().select();
+          }
+        },
+        {
+          text: "Select none",
+          action: function() {
+            table.rows().deselect();
+          }
+        }
+      ],
       oLanguage: {
         sLengthMenu: " _MENU_ ",
         sSearch: "",
@@ -174,20 +197,20 @@ $(document).ready(function() {
       responsive: true,
       bDestroy: true,
       select: true,
-        buttons: [
-            {
-                text: 'Select all',
-                action: function () {
-                    table.rows().select();
-                }
-            },
-            {
-                text: 'Select none',
-                action: function () {
-                    table.rows().deselect();
-                }
-            }
-        ],
+      buttons: [
+        {
+          text: "Select all",
+          action: function() {
+            table.rows().select();
+          }
+        },
+        {
+          text: "Select none",
+          action: function() {
+            table.rows().deselect();
+          }
+        }
+      ]
     });
     $("#basic-btn-5").DataTable({
       dom: "Bfrtip",
