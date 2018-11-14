a43aa477ae5d177d59e4b319726f25f3e7e1e526
libra89sanjay@gmail.com <libra89sanjay@gmail.com>
2018-11-14 15:21:41 +0530

    user dashboard fro Impact Study

--- CronusWebsite/Website/pages/assets/js/pages-js/impactStudy.js
+++ CronusWebsite/Website/pages/assets/js/pages-js/impactStudy.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
+  var html =
+'<div class="row"><div class="col-md-4">\
+<div class="select2-dd"></div>\
+</div>\
+<div class="col-md-6">\
+<input tabindex="1" type="text" placeholder="Artifact/JIRA ID" class="form-control">\
+<small class="form-text text-muted">Please provide space separated artifacts/JIRA for Impact Study\
+</small>\
+</div>\
+<div class="col-md-2 text-right">\
+<button class="btn btn-danger">\
+<i class="icofont icofont-trash"></i>\
+<span class="btn-text">Delete</span>\
+</button>\
+</div></div>';
   var width = $(window).width();
   if (width <= 768) {
     $("#basic-btn-1,#basic-btn-2,#basic-btn-3").DataTable({
@@ -28,7 +43,7 @@ $(document).ready(function() {
         .find("span")
         .text() == "Request Impact Study"
     ) {
-      $(this).html(
+      ~$(this).html(
         '<i class="icofont icofont-caret-left"></i><span>Back to Dashboard</span> '
       );
     } else {
@@ -38,5 +53,15 @@ $(document).ready(function() {
     }
     $(".impact-study-details").toggle();
     $(".impact-study-form").toggle();
+  });
+  $("#add-artifact-jira").on("click", function() {
+    $(".add-artifact-jira").append(html);
+    $(".select2-dd").select2({
+      placeholder: "Select",
+      allowClear: true
+    });
+  });
+  $(".add-artifact-jira button.btn-danger").on("click", function() {
+    $(this).parent().parent().remove();
   });
 });
